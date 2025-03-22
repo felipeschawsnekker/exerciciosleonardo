40098fa4cbde5817fc4d1cff68c667c11101a82d
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2025-03-22 20:59:40 -0300

    Create 9.cpp

---
+++ 9.cpp
@@ -0,0 +1,18 @@
+//9
+
+#include <iostream>
+using namespace std; 
+
+int main (){
+int num,result1,result2;
+  
+  cout<<"Choose a number: ";
+  cin>>num;
+
+result1=num+1;
+result2=num-1;
+
+  cout<<"Your result is: "<<result1<<endl;
+  cout<<"Your result is"<<result2<<endl;
+return 0;
+}
