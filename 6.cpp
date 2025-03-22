1ed8d032c83d145cc5e123ee0441131f0143f47a
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2025-03-22 20:57:34 -0300

    Create 6.cpp

---
+++ 6.cpp
@@ -0,0 +1,16 @@
+//6
+#include <iostream>
+using namespace std; 
+
+int main (){
+  int num,result;
+    cout<<"Choose a number: ";
+    cin>>num;
+
+result = num + 1;
+
+    cout<<"Your result is: "<<result;
+
+return 0;
+
+}
