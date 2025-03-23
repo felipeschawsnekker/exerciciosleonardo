9379b2832242a56d2378669fa52f9e78ec45a6dc
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2025-03-22 21:00:34 -0300

    Create 10.cpp

---
+++ 10.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+int main (){
+string address, number, name;
+
+  cout<<"What's your name?"<<endl;
+  cin>>name;
+
+  cout<<"What's your number?"<<endl;
+  if(!(cin>>number));
+
+  cout<<"What's your address?"<<endl;
+  cin>>address;
+
+  cout<<"Your name is "<<name<<endl;
+  cout<<"Your number is "<<number<<endl;
+  cout<<"Your address is "<<address<<endl;
+
+return 0;
+  
+}
