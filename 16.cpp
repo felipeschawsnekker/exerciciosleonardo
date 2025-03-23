8f35fb04319c9163c93ec8ed3b4f06f2af90e002
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2025-03-22 21:44:55 -0300

    Create 16.cpp

---
+++ 16.cpp
@@ -0,0 +1,54 @@
+//Ler um ângulo em graus e imprimir: seno, co-seno, tangente, secante, co-secante e co-tangente deste ângulo.
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main (){
+    double angulo,num,seno,cosseno,tangente,secante,cossecante,cotangente;
+    
+  
+    
+    cout<<"Digite um ângulo em graus: ";
+    cin>>angulo;
+    
+    num=(angulo*M_PI)/180;
+    
+    cout<<defaultfloat<<setprecision(8);
+    seno=sin(num);
+    cosseno=cos(num);
+    tangente=tan(num);
+    
+    cout<<"O seno é: "<<seno<<endl;
+    cout<<"O cosseno é: "<<cosseno<<endl;
+    cout<<"A tangente é: "<<tangente<<endl;
+    
+    if (seno!=0){
+        cossecante=1/seno;
+        cout<<"A cossecante é: "<<cossecante<<endl;
+    } 
+    else {
+        cout<<"Cossecante não pode ser calculada";
+    } 
+    
+    
+    if (cosseno!=0){
+       secante=1/seno;
+       cout<<"A secante é: "<<secante<<endl;
+    }
+    else {
+        cout<<"Secante não pode ser calculada";
+    }
+
+    if(tangente!=0){
+        cotangente=1/tangente;
+        cout<<"A cotangente é: "<<cotangente<<endl;
+    }
+    else {
+        cout<<"Cotangente nao pode ser calculada";
+    }
+    
+return 0;
+
+}
