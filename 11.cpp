41a5f9beefe7de3fe0b1891a4f5a15f02bb366e9
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2025-03-22 21:01:25 -0300

    Create 11.cpp

---
+++ 11.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int a,b,c;
+
+  cout<<"---ALGORITMO DE SOMA--- \n"<<endl;
+  cout<<endl;
+
+  cout<<"Primeiro número:";
+  cin>>a;
+
+  cout<<"Segundo número: ";
+  cin>>b;
+
+  c=a+b;
+
+  cout<<"A soma é: "<<c;
+  
+return 0;
+  
+}
