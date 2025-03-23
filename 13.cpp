c2a8da82df411c0c613876f10a2f451901cdfa01
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2025-03-22 21:02:53 -0300

    Create 13.cpp

---
+++ 13.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+double a,b;
+
+  cout<<"---ALGORITMO-- \n \n"<<endl;
+
+  cout<<"Número: ";
+  cin>>a;
+
+  b=a/3.0;
+  cout<<"O resultado é: "<<b;
+
+return 0;
+  
+}
+    
