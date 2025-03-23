94479f2e4b0ee984baa65ed04e4f25d3c669de07
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2025-03-22 21:02:20 -0300

    Create 12.cpp

---
+++ 12.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int a,b,c;
+
+  cout<<"---ALGORITMO DE PRODUTO--- \n \n"<<endl;
+
+  cout<<"Primeiro número: ";
+  cin>>a;
+
+  cout<<"Segundo numero: ";
+  cin>>b;
+
+  c=a*b;
+  cout<<"O produto é: "<<c;
+
+return 0;
+  
+}
+    
