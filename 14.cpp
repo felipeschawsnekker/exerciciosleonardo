cb70583ebbb9084c824bea005a915a71e6b99090
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2025-03-22 21:03:27 -0300

    Create 14.cpp

---
+++ 14.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+double a,b,c;
+
+  cout<<"---ALGORITMO DE MEDIA-- \n \n"<<endl;
+
+  cout<<"Primeiro número: ";
+  cin>>a;
+
+  cout<<"Ssgundo numero: ";
+  cin>>b;
+
+  c=a+b/2.0;
+  cout<<"A media é: "<<c;
+
+return 0;
+  
+}
+    
